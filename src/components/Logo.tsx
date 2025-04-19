96f6242f7b2679c0374ee65aa8aea0fc366c8aa5
Baron <136452254+Baron110904@users.noreply.github.com>
2025-04-19 11:42:54 +0100

    commit

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ const Logo = () => {
   return (
     <div className="w-32 h-32 mx-auto mb-8">
       <img
-        src="/placeholder.svg"
+        src="https://i.postimg.cc/633ZZMdB/Logo-Celtiis-100-MOI.jpg"
         alt="Logo de l'entreprise"
         className="w-full h-full object-contain"
       />

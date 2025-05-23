f2a4bc8dca48be689f15519dca3bde988d05f323
Baron <136452254+Baron110904@users.noreply.github.com>
2025-05-23 10:25:53 +0100

    Update WaaPage.tsx

--- src/pages/WaaPage.tsx
+++ src/pages/WaaPage.tsx
@@ -14,8 +14,8 @@ const WaaPage = () => {
         />
       </div>
 
-      {/* Contenu gauche (mobile: colonne entière) */}
-      <div className="w-full md:w-1/2 flex flex-col justify-between p-6 md:p-12 z-10 min-h-screen bg-white/90 md:bg-white">
+      {/* Contenu gauche (colonne entière sur mobile) */}
+      <div className="w-full md:w-1/2 flex flex-col justify-between p-6 md:p-12 z-10 min-h-screen md:bg-white">
         {/* Logo en haut */}
         <div className="mb-4">
           <img 
@@ -27,7 +27,7 @@ const WaaPage = () => {
 
         {/* Texte au milieu */}
         <div className="flex-1 flex items-center">
-          <h1 className="text-xl md:text-4xl font-normal leading-snug text-black">
+          <h1 className="text-xl md:text-4xl font-normal leading-snug text-white md:text-black">
             Télécharge waa, puis connecte-toi à la mini-app e-commerce et passe ta commande en quelques clics chez Royal Panini's.
           </h1>
         </div>
@@ -63,7 +63,7 @@ const WaaPage = () => {
               )
             }
           >
-            <svg xmlns="https://i.postimg.cc/NjPZwpT4/apple-store.png" width="18" height="18" viewBox="0 0 384 512">
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 384 512">
               <path
                 fill="white"
                 d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"

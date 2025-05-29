f2e34fa4f6fa8c80f882c47c69c8feef3a61d9b3
Baron <136452254+Baron110904@users.noreply.github.com>
2025-05-29 01:52:25 +0100

    Phoning

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -84,11 +84,16 @@ export default {
 					to: {
 						height: '0'
 					}
-				}
+				},
+				zoomInOut: {
+					'0%, 100%': { transform: 'scale(1)' },
+					'50%': { transform: 'scale(1.05)' },
+				},
 			},
 			animation: {
 				'accordion-down': 'accordion-down 0.2s ease-out',
-				'accordion-up': 'accordion-up 0.2s ease-out'
+				'accordion-up': 'accordion-up 0.2s ease-out',
+				'zoom-slow': 'zoomInOut 3s ease-in-out infinite'
 			},
 			fontFamily: {
         		'waa': ['"Lato"', 'sans-serif'], 

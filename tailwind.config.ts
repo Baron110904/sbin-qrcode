b789bcf358eb7ee25d0f75c1ea404a892165acda
Baron <136452254+Baron110904@users.noreply.github.com>
2025-05-29 01:39:48 +0100

    Phoning

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -91,7 +91,7 @@ export default {
 				'accordion-up': 'accordion-up 0.2s ease-out'
 			},
 			fontFamily: {
-        		'waa': ['"Abel"', 'sans-serif'], 
+        		'waa': ['"Lato"', 'sans-serif'], 
       		}
 		}
 	},

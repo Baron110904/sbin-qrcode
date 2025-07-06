14f89afdff92a6ff5b77c79968cc600a6cd4df3a
Baron <136452254+Baron110904@users.noreply.github.com>
2025-07-06 21:28:42 +0100

    commit

--- src/pages/WaaPage.tsx
+++ src/pages/WaaPage.tsx
@@ -89,7 +89,7 @@ const WaaPage = () => {
       </div>
 
       {/* Version Mobile - Plein écran avec espaces réduits */}
-      <div className="flex md:hidden flex-col min-h-screen">
+      <div className="flex md:hidden flex-col min-h-screen w-full overflow-hidden">
         {/* Section 1: Logo + Texte (18%) */}
         <div className="flex-none h-[15vh] bg-white flex items-center justify-start space-x-4 px-4 shadow-md z-10">
           <img 

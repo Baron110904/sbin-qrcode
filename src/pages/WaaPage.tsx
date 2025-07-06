0a5d51e80e777c57da3ad1db90afc55d3aa5cd4c
Baron <136452254+Baron110904@users.noreply.github.com>
2025-07-06 21:19:35 +0100

    commit

--- src/pages/WaaPage.tsx
+++ src/pages/WaaPage.tsx
@@ -148,7 +148,7 @@ const WaaPage = () => {
             Waa est la nouvelle super app de Celtiis qui remplace l'application MyCeltiis Cash. Elle regroupe tous les services de cette dernière avec plein d'autres innovations à l'instar de l'E-Commerce ; Le Ticketing - Scolarité - MyCeltiis - Près de moi...
           </p>
         </div>
-        <div className="h-4 bg-white" />
+        <div className="h-10 bg-white" />
       </div>
     </div>
   );
